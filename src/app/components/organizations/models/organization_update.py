c08ba3d7cd165a01fc3fdf406564081ae50c460e
solvemate2018 <georgy.d.2001@gmail.com>
2024-11-08 09:17:39 +0100

    Removed placeholder componenents

--- src/app/components/organizations/models/organization_update.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class OrganizationUpdateModel(BaseModel):
-    organization_name: str = None
-    organization_email: str = None
-    first_name: str = None
-    last_name: str = None
--- src/app/components/organizations/utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-async def some_util_func():
-    logging.info("Utility Function")
-
-
-async def some_back_processing_func():
-    logging.info("Back Processing Function")
